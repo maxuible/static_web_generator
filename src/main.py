a1a82fdea1ffda15cb4aaa70c722b2dc176d5954
Max Uible <maxuible@gmail.com>
2025-04-23 20:09:48 -0500

    before remove function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from textnode import TextNode
 from textnode import TextType
 
+import shutil
+
 def main():
     node = TextNode("this is some anchor text", TextType.NORMAL, "https://boot.dev")
 
     print(node)
-    
+    shutil.rmtree()
 
 
 main()
